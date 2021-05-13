755d0894b55f2df875c3764db3bf61f8a3fdd653
Arkajyoti Bhattacharjee <arkastat98@gmail.com>
2021-05-13 20:16:28 +0530

    mwg_try_2.0

--- Final/mwg_try.R
+++ Final/mwg_try.R
@@ -1,31 +1,28 @@
-
 library(tictoc)
 
-K <- 10        # no of theta parameters
-R <- seq(5, 95, by = 10)
+K <- 10       # no of theta parameters
+R <- round(seq(5, 500, length.out = K))
 sumR <- c(0, cumsum(R))
 
 Y <- numeric(0)
 
-for(i in 1:K){
-	Y <- c(Y, rnorm(R[i], mean = i - 1, sd = 10))
-}
-
+for(i in 1:K) Y <- c(Y, rnorm(R[i], mean = i - 1, sd = 10))
 
 B <- 1e5
 
 ###########  Declaration and Initialization of parameter vectors
 
 A <- rep(0, 50*B + 1)
-A[1] <- 0.5
+#A[1] <- 0.5
+A[1] <- 1
 
 u <- rep(0, 50*B + 1)
 
 theta <- matrix(0, nrow = 50*B + 1, ncol = K)
 
 V <- rep(0, 50*B + 1)
-V[1] <- (sum((Y - rep(theta[1, ], R))^2)/2 + 1)/(sum(R)/2 + 1)
-
+#V[1] <- (sum((Y - rep(theta[1, ], R))^2)/2 + 1)/(sum(R)/2 + 1)
+V[1] <- 1
 
 #################################################################
 
@@ -38,14 +35,18 @@ tic()
 
 for(n in 1:B){
 
-	if(n%%10000 == 0) print(n)
+	if(n%%1000 == 0) print(n)
 
 	for(i in 2:51){
 
 		j = (n - 1)*50 + i
 		y <- rnorm(K+2, mean = c(A[j-1], u[j-1], theta[j-1, ]), sd = sig)
 		z <- log(runif(K+2))
+		
+		### Updating V
 
+		temp <- rgamma(1, shape = sum(R)/2 + 1, rate = sum((Y - rep(theta[j-1, ], R))^2)/2 + 1)
+		V[j] <- 1/temp
 
 		### Updating A
 
@@ -54,7 +55,8 @@ for(n in 1:B){
 		}
 		else{
 			r <- (2*K - 2)*(log(y[1]) - log(A[j-1])) + (1/A[j-1] - 1/y[1]) + 
-					sum(log(A[j-1]^2 + (theta[j-1, ] - u[j-1])^2)) - sum(log(y[1]^2 + (theta[j-1, ] - u[j-1])^2))
+					sum(log(A[j-1]^2 + (theta[j-1, ] - u[j-1])^2)) -
+					 sum(log(y[1]^2 + (theta[j-1, ] - u[j-1])^2))
 			
 			if(z[1] < r){
 				A[j] <- y[1]
@@ -66,13 +68,14 @@ for(n in 1:B){
 		
 		### Updating V
 
-		temp <- rgamma(1, shape = sum(R)/2 + 1, rate = sum((Y - rep(theta[j, ], R))^2)/2 + 1)
-		V[j] <- 1/temp
+##		temp <- rgamma(1, shape = sum(R)/2 + 1, rate = sum((Y - rep(theta[j, ], R))^2)/2 + 1)
+##		V[j] <- 1/temp
 
 
 		### Updating u
 
-		r <- (u[j-1]^2 - y[2]^2)/2 + sum(log(A[j]^2 + (theta[j-1, ] - u[j-1])^2)) - sum(log(A[j]^2 + (theta[j-1, ] - y[2])^2))
+		r <- (u[j-1]^2 - y[2]^2)/2 + sum(log(A[j]^2 +
+			 (theta[j-1, ] - u[j-1])^2)) - sum(log(A[j]^2 + (theta[j-1, ] - y[2])^2))
 
 		if(z[2] < r){
 			u[j] <- y[2]
@@ -110,7 +113,16 @@ toc()
 
 print(p/(50*B))
 
-for(k in 1:3)  ts.plot(ls[, 2+k], type = "l")
+pdf("MWG_try.pdf")
+for(k in 1:K)  ts.plot(ls[, k], type = "l")
+dev.off()
+
+pdf("MWG_params.pdf")
+ts.plot(A[1:1e5])
+ts.plot(V[1:1e5])
+ts.plot(u[1:1e5])
+for(k in 1:K)  ts.plot(theta[1:1e5, k], type = "l")
+dev.off()
 
 
 
